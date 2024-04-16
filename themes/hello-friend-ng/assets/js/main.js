ecd961f046aa0669c1f83f837078a591d4b667cd
Delluci <wmn.brunenberg@student.avans.nl>
2024-04-16 15:14:36 +0200

    feat(Theme): instantanious dark/light mode switch
    Changes to main.js so the site doesn't need a reload for changing the theme. This makes for a better user experience 🎉

--- themes/hello-friend-ng/assets/js/main.js
+++ themes/hello-friend-ng/assets/js/main.js
@@ -7,51 +7,22 @@
  */
 const themeToggle = document.querySelector(".theme-toggle");
 const chosenTheme = window.localStorage && window.localStorage.getItem("theme");
-const chosenThemeIsDark = chosenTheme == "dark";
-const chosenThemeIsLight = chosenTheme == "light";
+const chosenThemeIsDark = chosenTheme === "dark";
 
-// Detect the color scheme the operating system prefers.
-function detectOSColorTheme() {
-  if (chosenThemeIsDark) {
-    document.documentElement.setAttribute("data-theme", "dark");
-  } else if (chosenThemeIsLight) {
-    document.documentElement.setAttribute("data-theme", "light");
-  } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    document.documentElement.setAttribute("data-theme", "dark");
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-  }
-}
+// Apply the initial theme.
+document.documentElement.setAttribute("data-theme", chosenThemeIsDark ? "dark" : "light");
 
 // Switch the theme.
-function switchTheme(e) {
-  if (chosenThemeIsDark) {
-    localStorage.setItem("theme", "light");
-  } else if (chosenThemeIsLight) {
-    localStorage.setItem("theme", "dark");
-  } else {
-    if (document.documentElement.getAttribute("data-theme") == "dark") {
-      localStorage.setItem("theme", "light");
-    } else {
-      localStorage.setItem("theme", "dark");
-    }
-  }
+function switchTheme() {
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+  const newTheme = currentTheme === "dark" ? "light" : "dark";
 
-  detectOSColorTheme();
-  window.location.reload();
+  document.documentElement.setAttribute("data-theme", newTheme);
+  window.localStorage && window.localStorage.setItem("theme", newTheme);
 }
 
 // Event listener
 if (themeToggle) {
   themeToggle.addEventListener("click", switchTheme, false);
-  window
-    .matchMedia("(prefers-color-scheme: dark)")
-    .addEventListener("change", (e) => e.matches && detectOSColorTheme());
-  window
-    .matchMedia("(prefers-color-scheme: light)")
-    .addEventListener("change", (e) => e.matches && detectOSColorTheme());
-
-  detectOSColorTheme();
-} else {
-  localStorage.removeItem("theme");
 }
+
